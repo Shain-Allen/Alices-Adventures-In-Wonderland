0d30906be859ca651c7c4467fe6fe79375eed1b4
Shain-Allen <48696021+Shain-Allen@users.noreply.github.com>
2020-03-02 21:26:12 -0800

    H

---
+++ The Game/Alice's Battle's in Wonderland/Assets/Scripts/EnemySpawnManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawnManager : MonoBehaviour
+{
+    public GameObject[] enemySpawners;
+
+    //in seconds
+    public float spawnIntervel = 5.0f;
+    float timeTillNextSpawn;
+
+    private void Start()
+    {
+        timeTillNextSpawn = spawnIntervel;
+    }
+
+    private void Update()
+    {
+        timeTillNextSpawn -= Time.deltaTime;
+
+        if (timeTillNextSpawn <= 0)
+        {
+            int r = Random.Range(0, enemySpawners.Length);
+
+            enemySpawners[0].GetComponent<SpawnPointController>().SpawnEnemy();
+
+            timeTillNextSpawn = spawnIntervel;
+        }
+
+        //Debug.Log(timeTillNextSpawn);
+    }
+}
