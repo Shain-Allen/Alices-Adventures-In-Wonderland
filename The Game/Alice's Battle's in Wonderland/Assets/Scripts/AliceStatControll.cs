db7935150c08279377a4c6d2ff3937e66570dc9d
Shain-Allen <48696021+Shain-Allen@users.noreply.github.com>
2020-03-08 18:06:32 -0700

    fixed alice scaleing
    switched alice to scale with animation instead of doing it via code

--- The Game/Alice's Battle's in Wonderland/Assets/Scripts/AliceStatControll.cs
+++ The Game/Alice's Battle's in Wonderland/Assets/Scripts/AliceStatControll.cs
@@ -13,29 +13,19 @@ public class AliceStatControll : MonoBehaviour
     public Sprite emptyHeart;
 
     //size stuff
-    public float startingSizeScale = 1f;
-    public float scaleChangeDuration = 5.0f;
-    float currentSizeScale;
-    public float nextSizeScale;
+    
 
     //other stuff
     float starttime;
 
     private void Start()
     {
-        currentSizeScale = startingSizeScale;
-        nextSizeScale = startingSizeScale;
-
         starttime = Time.time;
     }
 
     private void FixedUpdate()
     {
-        if (currentSizeScale != nextSizeScale)
-        {
-            float t = (Time.time - starttime) / scaleChangeDuration;
-            transform.localScale = new Vector2(Mathf.SmoothStep(currentSizeScale, nextSizeScale, t), Mathf.SmoothStep(currentSizeScale, nextSizeScale, t));
-        }
+        
     }
 
     private void Update()
@@ -46,12 +36,6 @@ public class AliceStatControll : MonoBehaviour
     public void TakeDamage(int damage)
     {
         health -= damage;
-    }
-
-    public void ChangeSize(float NewSize)
-    {
-        nextSizeScale = currentSizeScale;
-        nextSizeScale += NewSize;
     }
 
     void heartControl()
