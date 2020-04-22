6520abb72f35490b291ec1783eee46ef52f720c4
Shain-Allen <48696021+Shain-Allen@users.noreply.github.com>
2020-04-21 19:37:10 -0700

    Finished up game for turn in

---
+++ The Game/Alice's Battle's in Wonderland/Assets/Scripts/MenuStuff/ButtonController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonController : MonoBehaviour
+{
+    public string lastScene;
+    public string nextScene;
+
+    public void BackButton()
+    {
+        SceneManager.LoadScene(lastScene);
+    }
+
+    public void NextButton()
+    {
+        SceneManager.LoadScene(nextScene);
+    }
+}
